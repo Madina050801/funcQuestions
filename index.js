cefd1d0607e9d28b326e1331b09334a0feac42c6
abduraufova_ <92726131+Madina050801@users.noreply.github.com>
2021-11-27 23:33:26 +0500

    ques

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
-// function obj(num1, num2) {
-//     if (num1, num2) {
-//         console.log(num1 + num2);
-//     }
-//     return function (num3) {
-//         console.log(num1 + num3);
-//     }
+// console.log("2+2" * 3);
+
+// function abc(a = 1, b = 2) {
+//     console.log(a + b);
 // }
-// obj(1, 2);
-// obj(1)(4);
+
+// abc(8);
+
+// const array = [0, 5, 7, 1, 2, 2, 3, 4, 5, 5];
+// let count
+// array.sort((a, b) => a - b);
+// for (i = 0; i < array.length; i++) {
+//     if (array[i] == array[i + 1]) {
+//         count++;
+//     }
+//     if (count != 0) {
+//         array.splice(i, count)
+//     }
+//     count = 0;
+// }
+// console.log(array);
+
+// function getData(arr, num) {
+//     let count = 0;
+//     let sum = 0;
+
+//     for (i = 0; i < arr.length; i++) {
+//         for (j = i; j < arr.length; j++) {
+//             sum += arr[j]
+
+//             if (sum / (j - i + 1) == num) {
+//                 count++;
+//                 console.log(sum,  (j - i + 1));
+//             }
+//         }
+//         sum = 0;
+//     }
+//     console.log(count);
+// }
+// getData([1, 3, 2, 2], 2);
+
+let str = "AAABBD";
+list = []
+count = 1
+
+for (i = 0; i < str.length; i++) {
+    if (str[i] == str[i + 1]) {
+        count++;        
+    } 
+    else {
+        var value = `${str[i]}${count == 1? '': count}`
+        list = [...list, value]
+        count = 1
+        // strNew += str[i] + (count + 1);
+    }
 
 
-
-const obj1 = {
-    a: 1,
-    b: 2,
-    getA() {
-        console.log(this.a);
-        return this
-    },
-    getB() {
-        console.log(this.b);
-        // return this
-    },
+    // if (count != 0) strNew += str[i] + (count + 1)
+    // else strNew += str[i];
 }
-obj1.getA().getB();
+console.log(list.join(''));
+
