3fd391bb108b89adb1d2dea28e24b1ccee0b8a01
abduraufova_ <92726131+Madina050801@users.noreply.github.com>
2021-12-05 22:20:40 +0500

    reexam

--- index.js
+++ index.js
@@ -39,24 +39,75 @@
 // }
 // getData([1, 3, 2, 2], 2);
 
-let str = "AAABBD";
-list = []
-count = 1
+// let str = "AAABBD";
+// list = []
+// count = 1
 
-for (i = 0; i < str.length; i++) {
-    if (str[i] == str[i + 1]) {
-        count++;        
-    } 
-    else {
-        var value = `${str[i]}${count == 1? '': count}`
-        list = [...list, value]
-        count = 1
-        // strNew += str[i] + (count + 1);
-    }
+// for (i = 0; i < str.length; i++) {
+//     if (str[i] == str[i + 1]) {
+//         count++;        
+//     } 
+//     else {
+//         var value = `${str[i]}${count == 1? '': count}`
+//         list = [...list, value]
+//         count = 1
+//         // strNew += str[i] + (count + 1);
+//     }
 
 
-    // if (count != 0) strNew += str[i] + (count + 1)
-    // else strNew += str[i];
-}
-console.log(list.join(''));
+//     // if (count != 0) strNew += str[i] + (count + 1)
+//     // else strNew += str[i];
+// }
+// console.log(list.join(''));
+
+
+// const title = [
+//     {
+//         id:1,
+//         name:'Asadaaa',
+//         status:'Students',
+//     }
+// ];
+// a =0;
+// s = 0;
+// for (let i = 0; i < title.length; i++) {
+// NewNAme = title[0].name;
+// NewStatus = title[0].status;
+// for (let j = 0; j < NewNAme.length; j++) {
+//     (NewNAme[j] === 'a' || NewNAme[j] === 'A') && a++;
+// }
+// for (let k = 0; k < NewStatus.length; k++) {
+//     (NewStatus[k] === 's' || NewStatus[k] === 'S') && s++;
+// }
+// }
+// console.log(a,'=a');
+// console.log(s,'=s');
+
+// 1
+const title = 'WebBrain Academy Number One'
+const res = title.split('').filter((value) => (value === value.toUpperCase() && value != ' ') );
+console.log(res.join(''));
+
+// 2
+const array = [4, 2, 6, 16, 27, 81];
+console.log(array.filter((value) => Math.sqrt(value) % 1 ===0 ));
+
+// 3
+//2-xonali uchun
+const numbers = [4, 2, 6, 16, 27, 81, 3456];
+console.log(
+    numbers.map((value) => {
+        if(`${value}`.length ===1) {
+            return value;
+        } else {
+            let temp = `${value}`;
+            temp = `${temp[1]}${temp[0]}`;
+            return parseInt(temp)
+        }
+    })
+);
+//universal yechim
+console.log(numbers.map((value) => parseInt(`${value}`.split('').reverse().join(''))));
+
+
 
